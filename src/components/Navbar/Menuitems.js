5e5e5d5db0d958fda432c0d6328d0690830e6a80
Kswierk <kacper.swierk@gmail.com>
2020-09-04 11:58:20 +0200

    Added navbar

---
+++ src/components/Navbar/Menuitems.js
@@ -0,0 +1,24 @@
+
+
+export const MenuItems = [
+    {
+        title: 'Strona główna',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Oferta',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'O mnie',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Kontakt',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
